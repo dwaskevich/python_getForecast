703b3b52b0ab93ac9017bb2aed58521074a0cda5
David Waskevich <dwaskevich@outlook.com>
2024-09-29 21:24:42 -0500

    removed some unwanted pieces of data

--- getForecast.py
+++ getForecast.py
@@ -6,6 +6,7 @@ from retry_requests import retry
 
 zipcode = input('Input zip code: ')
 
+# get geolocation coordinates (latitude/longitude) for requested zip code
 nomi = pgeocode.Nominatim('us')
 location = nomi.query_postal_code(zipcode)
 print(location.latitude, location.longitude)
@@ -16,57 +17,17 @@ retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
 openmeteo = openmeteo_requests.Client(session = retry_session)
 
 # Make sure all required weather variables are listed here
-# The order of variables in hourly or daily is important to assign them correctly below
 url = "https://api.open-meteo.com/v1/forecast"
 params = {
 	"latitude": location.latitude,
 	"longitude": location.longitude,
-	"current": ["precipitation", "rain", "showers"],
-	"hourly": ["temperature_2m", "precipitation_probability", "rain"],
 	"daily": "precipitation_probability_max",
-	"temperature_unit": "fahrenheit",
-	"wind_speed_unit": "mph",
-	"precipitation_unit": "inch",
-	"timezone": "America/Chicago"
 }
 responses = openmeteo.weather_api(url, params=params)
 
 # Process first location. Add a for-loop for multiple locations or weather models
 response = responses[0]
 print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
-print(f"Elevation {response.Elevation()} m asl")
-print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
-print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
-
-# Current values. The order of variables needs to be the same as requested.
-current = response.Current()
-current_precipitation = current.Variables(0).Value()
-current_rain = current.Variables(1).Value()
-current_showers = current.Variables(2).Value()
-
-print(f"Current time {current.Time()}")
-print(f"Current precipitation {current_precipitation}")
-print(f"Current rain {current_rain}")
-print(f"Current showers {current_showers}")
-
-# Process hourly data. The order of variables needs to be the same as requested.
-hourly = response.Hourly()
-hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
-hourly_precipitation_probability = hourly.Variables(1).ValuesAsNumpy()
-hourly_rain = hourly.Variables(2).ValuesAsNumpy()
-
-hourly_data = {"date": pd.date_range(
-	start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),
-	end = pd.to_datetime(hourly.TimeEnd(), unit = "s", utc = True),
-	freq = pd.Timedelta(seconds = hourly.Interval()),
-	inclusive = "left"
-)}
-hourly_data["temperature_2m"] = hourly_temperature_2m
-hourly_data["precipitation_probability"] = hourly_precipitation_probability
-hourly_data["rain"] = hourly_rain
-
-hourly_dataframe = pd.DataFrame(data = hourly_data)
-print(hourly_dataframe)
 
 # Process daily data. The order of variables needs to be the same as requested.
 daily = response.Daily()
